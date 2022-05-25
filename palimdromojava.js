f8e7dd2664c6013f97028fde918b2646ea97cc2d
vinyand <vinyand2@gmail.com>
2022-05-25 00:24:19 -0300

    Palimdromos e trocando Pares no array

---
+++ palimdromojava.js
@@ -0,0 +1,27 @@
+//solução para palimdromo
+function verificaPalindromo(string) {
+    if (!string) return;
+
+    return string.split("").reverse().join("") === string; //split separa todas as letras
+}
+
+console.log(verificaPalindromo("ovo"));
+
+
+-
+- -- - - - - -
+// solução 2
+function verificaPalindromo2(string) {
+    if (!string) return "string inexistente";
+
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+
+    }
+
+    return true;
+}
+
+console.log(verificaPalindromo2("abbba"));
